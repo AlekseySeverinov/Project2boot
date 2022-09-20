05cb9c28e9846924b0c98c7ead7f2be3e44c4b6e
AlekseySeverinov <alekseyseverinov2212@gmail.com>
2022-09-20 14:45:33 +0300

    Initial commit

---
+++ src/main/java/springcourse/Project2Boot/dao/PersonDAO.java
@@ -0,0 +1,7 @@
+package springcourse.Project2Boot.dao;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PersonDAO {}
+
---
+++ src/main/java/springcourse/Project2Boot/repositories/PeopleRepository.java
@@ -0,0 +1,13 @@
+package springcourse.Project2Boot.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import springcourse.Project2Boot.models.Person;
+
+
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository <Person, Integer> {
+    Optional <Person> findByFullName (String fullName);
+}
